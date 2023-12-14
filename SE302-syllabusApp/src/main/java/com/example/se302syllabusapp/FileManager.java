d495e2d8a33eae2709fcd55d7439a476caac1617
haro3535 <66617253+haro3535@users.noreply.github.com>
2023-12-15 01:12:33 +0300

    read() function is done json file is formatted

--- SE302-syllabusApp/src/main/java/com/example/se302syllabusapp/FileManager.java
+++ SE302-syllabusApp/src/main/java/com/example/se302syllabusapp/FileManager.java
@@ -4,12 +4,16 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import javax.json.Json;
+import javax.json.JsonObject;
 import javax.json.stream.JsonParser;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.json.simple.parser.*;
 
 
@@ -70,30 +74,86 @@ public class FileManager {
         // TODO: We have to figure it out how to pass paths of JSON and TXT files!
         String filepath = "fake_path";
         // Take JSON file
+        JSONParser parser = new JSONParser();
 
+        SyllabusData root = new SyllabusData();
 
         try {
-            //JSONObject jsonObject = (JSONObject) parser.parse(new FileReader("storage/en/CE216/Deneme-JSON-V1.json"));
+            JSONObject jsonObject = (JSONObject) parser.parse(new FileReader("storage/en/CE216/Deneme-JSON-V1.json"));
 
-            JsonParser parser1 = Json.createParser(new FileReader("storage/en/CE216/Deneme-JSON-V1.json"));
+            Object keyName = jsonObject.keySet();
+            String[] name = keyName.toString().split("[\\[\\]]");
 
-            SyllabusData root = new SyllabusData();
-            //JSONArray root1 = (JSONArray) jsonObject.get("syllabus");
-            JsonParser.Event event = parser1.next(); // START_OBJECT
-            System.out.println(event);
+            root.setName(name[1]);
 
+            JSONArray children = (JSONArray) jsonObject.get(name[1]);
 
-            // Root's children of the JSON file
-            //JSONArray sections = (JSONArray) jsonObject.get("syllabus");
+            for(Object sub: children) {
 
+                SyllabusData subObject = new SyllabusData();
+                // GI , WSRM , A ...
+                JSONObject jObj = (JSONObject) sub;
 
+                String subObjectName = jObj.keySet().toString().split("[\\[\\]]")[1];
 
+                subObject.setName(subObjectName);
 
-        } catch (IOException e) {
+                JSONArray jA1 = (JSONArray) jObj.get(subObjectName);
+
+                for (Object sub1: jA1) {
+                    // GI , WSRM , A ... bunların içindeki objeler
+                    JSONObject jObj1 = (JSONObject) sub1;
+
+                    if (jObj1.isEmpty())
+                        continue;
+
+                    SyllabusData subObject1 = new SyllabusData();
+
+                    String subObjectName1 = jObj1.keySet().toString().split("[\\[\\]]")[1];
+
+                    subObject1.setName(subObjectName1);
+
+                    try {
+                        JSONArray sub2 = (JSONArray) jObj1.get(subObjectName1);
+
+                        for (Object sub2Elements: sub2) {
+
+                            JSONObject sub3 = (JSONObject) sub2Elements;
+
+                            SyllabusData subObject2 = new SyllabusData();
+                            if (sub3.keySet().toString().split("[\\[\\]]")[1].isEmpty())
+                                subObject2.setName(sub3.keySet().toString().split("[\\[\\]]")[1]);
+
+                            System.out.println(sub3.keySet().toString().split("[\\[\\]]")[1]);
+
+                            for (Object kName: sub3.keySet().toArray()) {
+                                SyllabusData subObject3 = new SyllabusData();
+                                subObject3.setName(kName.toString());
+                                subObject3.setValue(sub3.get(kName.toString()).toString());
+                                subObject2.getChildren().add(subObject3);
+                            }
+                            subObject1.getChildren().add(subObject2);
+
+                        }
+
+                    }catch (Exception e) {
+                        // Özel bir durum Total için bir alt çocuğu olmayanlar için
+                        subObject1.setValue(jObj1.get(subObjectName1).toString());
+
+                    }
+                    subObject.getChildren().add(subObject1);
+
+                }
+
+                // Burada bir alt sub objeclerin ekelemelerini yapıyorum
+                root.getChildren().add(subObject);
+
+            }
+        } catch (IOException | ParseException e) {
             throw new RuntimeException(e);
         }
 
-        return new SyllabusData();
+        return root;
     }
 
 
