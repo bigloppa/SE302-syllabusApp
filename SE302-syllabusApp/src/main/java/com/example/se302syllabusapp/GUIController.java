fe35bd1ecf1e3258e0ea7ba212a7e404cb341248
haro3535 <66617253+haro3535@users.noreply.github.com>
2023-12-16 16:54:22 +0300

    Some GUI functions added. FXML files and controllers are updated.

--- SE302-syllabusApp/src/main/java/com/example/se302syllabusapp/GUIController.java
+++ SE302-syllabusApp/src/main/java/com/example/se302syllabusapp/GUIController.java
@@ -1,30 +1,38 @@
 package com.example.se302syllabusapp;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Popup;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GUIController implements Initializable {
+    @FXML
+    private HBox parentHBox;
     FileManager fileManager;
-    FXMLLoader loader;
+    // Todo bunlari dosyala
+    Parent root;
     Controllers controllers;
     VersionController versionController;
 
 
 
-    public void handelImportEvent(ActionEvent event){
+    public void importSyllabus(ActionEvent event){
 
         // File Dialog here
         //getClass().getPackage().get
         File storagePath = new File("storage");
         if (!storagePath.exists()) {
-            System.out.println("File does not exist!");
+            System.out.println("Storage file does not exist!");
             return;
         }
 
@@ -36,19 +44,40 @@ public class GUIController implements Initializable {
         );
         File seletcetFile = chooser.showOpenDialog(new Popup());
 
-
-        System.out.println(seletcetFile.getAbsolutePath());
-
+        // Buralar deneme amacli yazildi kod calisiyor ama duzenlenmesi gerek
         setFileManager(new FileManager());
-        getFileManager().setJsonFile(new File(seletcetFile.getAbsolutePath()));
+        SyllabusData syllabusData = new SyllabusData();
+        if (seletcetFile != null) {
+            getFileManager().setJsonFile(new File(seletcetFile.getAbsolutePath()));
+            syllabusData = getFileManager().read();
+        }
 
-        SyllabusData syllabusData = getFileManager().read();
+
+
 
         System.out.println(syllabusData.name);
 
         //getFileManager().setJsonFile(new File(""));
 
         //SyllabusData syllabusData = getFileManager().read();
+
+    }
+
+    public void addingSyllabus() {
+
+        FXMLLoader syllabusLoader = new FXMLLoader(getClass().getResource("SyllabusSheet.fxml"));
+
+        try {
+            Node syllabusSheet = syllabusLoader.load();
+
+            if (!parentHBox.getChildren().isEmpty())
+                parentHBox.getChildren().remove(0);
+
+            parentHBox.getChildren().add(syllabusSheet);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -63,13 +92,13 @@ public class GUIController implements Initializable {
     public void setFileManager(FileManager fileManager) {
         this.fileManager = fileManager;
     }
-
-    public FXMLLoader getLoader() {
-        return loader;
+    // Todo bunlari dosyala
+    public Parent getRoot() {
+        return root;
     }
 
-    public void setLoader(FXMLLoader loader) {
-        this.loader = loader;
+    public void setRoot(Parent root) {
+        this.root = root;
     }
 
     public Controllers getControllers() {
@@ -87,7 +116,6 @@ public class GUIController implements Initializable {
     public void setVersionController(VersionController versionController) {
         this.versionController = versionController;
     }
-
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
