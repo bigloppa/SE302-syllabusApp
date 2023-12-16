86d206a1e889f1eedbd02be0c8c2b934069bcdaf
Ege Deniz Yaşar <91566475+egedenizz@users.noreply.github.com>
2023-12-16 15:19:54 +0300

    created save method

--- SE302-syllabusApp/src/main/java/com/example/se302syllabusapp/Controllers.java
+++ SE302-syllabusApp/src/main/java/com/example/se302syllabusapp/Controllers.java
@@ -1,6 +1,11 @@
 package com.example.se302syllabusapp;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 
 public class Controllers extends FileManager{
@@ -31,6 +36,21 @@ public class Controllers extends FileManager{
 
 
     public void Save(ArrayList<File> files,String filePath){
+        for (File file: files){
+            String sourceFilePath = file.getPath();
 
+
+            Path sourcePath = Paths.get(sourceFilePath);
+            Path destinationPath = Paths.get(filePath);
+
+            // Copy the file
+            try {
+                Files.copy(sourcePath, destinationPath, StandardCopyOption.REPLACE_EXISTING);
+
+                System.out.println("File copied successfully to: " + filePath);
+            } catch (IOException e) {
+                System.out.println("An error occurred while copying the file: " + e.getMessage());
+            }
+        }
     }
 }
