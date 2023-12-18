79f6c73477d03114a185ca6cb249511d63b9cbd8
Ege Deniz Yaşar <91566475+egedenizz@users.noreply.github.com>
2023-12-18 18:00:49 +0300

    wrote the codde for update method

--- SE302-syllabusApp/src/main/java/com/example/se302syllabusapp/Controllers.java
+++ SE302-syllabusApp/src/main/java/com/example/se302syllabusapp/Controllers.java
@@ -3,9 +3,10 @@ package com.example.se302syllabusapp;
 import javafx.stage.FileChooser;
 import javafx.stage.Popup;
 import javafx.stage.Stage;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -30,12 +31,47 @@ public class Controllers extends FileManager{
 
     }
 
-    public void update(){
+    // TODO: 18.12.2023 it needs to be tested 
+    public void update(String filePath){
+        JSONObject courseObject = new JSONObject();
+        JSONObject subObject = new JSONObject();
+        JSONArray syllabus = new JSONArray();
+        delete(filePath);
+        for(SyllabusData syllabusData: syllabusData1.getChildren()){
+            for (SyllabusData syllabusData3 : syllabusData.getChildren()){
+                for (SyllabusData syllabusData4 : syllabusData3.getChildren()){
+                    
+                    courseObject.put(syllabusData4.getName(), syllabusData4.getValue());
+                    
+                }
+                
+                subObject.put(syllabusData3.getName(),courseObject);
+                
+            }
 
+            syllabus.add(subObject);
+            
+        }
+
+        try (FileWriter file = new FileWriter(filePath)) {
+            file.write(syllabus.toJSONString());
+            System.out.println("JSON data written to file successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        
     }
 
-    public void delete(){
+    public void delete(String filePath){
+        File file = new File(filePath);
 
+
+        if (file.delete()) {
+            System.out.println("File deleted successfully.");
+        } else {
+            System.out.println("Failed to delete file!");
+        }
     }
 
     public void fileExport(File file, Stage primaryStage){
